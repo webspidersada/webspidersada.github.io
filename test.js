82793f056b4592c9e55a1880661dc142660b2c83
webspidersada <95533427+webspidersada@users.noreply.github.com>
2021-12-05 15:23:46 +0400

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 function myFunction() {
-  alert("When you click on the name of a student in the table, the Code Academy profile of that student will appear on a new tab. Additionally, clicking on the Gmail icon will copy our email address to your clipboard!");
+  alert("When you click on the name of a student in the table, the Codecademy profile of that student will appear on a new tab. Additionally, clicking on the Gmail icon will copy our email address to your clipboard!");
 }
